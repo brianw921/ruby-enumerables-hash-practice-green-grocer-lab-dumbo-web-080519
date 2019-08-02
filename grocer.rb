13dad144bd21bccb18c626c0a2f3d2a1c6f94d2a
Brian Wong <bwong92@gmail.com>
2019-08-02 22:54:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -56,11 +56,8 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  #call the consolidate to get the count item first
   new_cart = consolidate_cart(cart)
-  #apply coupon to the new cart
   apply_coupons(new_cart, coupons)
-  #apply clearance after discount from coupon
   apply_clearance(new_cart)
 
 total = 0
