a38cbea807e7e2c760467cff4a6c4623c692bb54
Brian Wong <bwong92@gmail.com>
2019-08-02 21:43:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,40 @@
+require 'pry'
 def consolidate_cart(cart)
   # code here
+  new_cart = {}
+  cart.each do |hash|
+    hash.each do |name, describe|
+
+      if new_cart[name]
+        new_cart[name][:count] += 1
+
+      else
+        new_cart[name] = describe
+        new_cart[name][:count] = 1
+    end
+  end
+  new_cart
+
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+
+
+
 end
 
 def apply_clearance(cart)
   # code here
+  discount = 0.80
+  cart.each do |item, details|
+
+    if cart[details][:clearance] == true
+      cart[details][:price] = (cart[details][:price]*discount).round(1)
+    end
+  end
+  cart
+  binding.pry
 end
 
 def checkout(cart, coupons)
