cfa45dc9c578cef13c80f56d3b3301cff4a72d3f
Brian Wong <bwong92@gmail.com>
2019-08-02 22:04:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -51,35 +51,5 @@ def apply_coupons(cart, coupons)
      end
    end
    new_cart
-   
-end
 
-def apply_clearance(cart)
-  new_cart = cart
-  cart.each do |name, hash|
-      if hash[:clearance]
-        new_cart[name][:price] = (cart[name][:price] * 0.8).round(1)
-      end
-  end
-  new_cart
-end
-
-def checkout(cart, coupons)
-  #call the consolidate to get the count item first
-  new_cart = consolidate_cart(cart)
-  #apply coupon to the new cart
-  apply_coupons(new_cart, coupons)
-  #apply clearance after discount from coupon
-  apply_clearance(new_cart)
-
-total = 0
-  new_cart.each do |name, hash|
-    total += (hash[:price] * hash[:count])
-  end
-
-if total >= 100
-    total *= 0.9
-  end
-
-  total
 end
