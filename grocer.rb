f3d8533f3c4be6dc6c5a9b4cabab7c755bd83cf8
Brian Wong <bwong92@gmail.com>
2019-08-02 22:32:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -32,7 +32,7 @@ def apply_coupons(cart, coupons)
 
        new_cart[name][:count] -= coupon_num
        if new_cart["#{name} W/COUPON"]
-         new_cart["#{name} W/COUPON"][:price] =
+         new_cart["#{name} W/COUPON"][:price] = coupon_cost
          new_cart["#{name} W/COUPON"][:count] = coupon_num
 
        else
