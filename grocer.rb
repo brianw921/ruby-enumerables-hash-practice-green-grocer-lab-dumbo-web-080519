74076e9b8a01639f6ee3b3b7d3fe109f6216ee25
Brian Wong <bwong92@gmail.com>
2019-08-02 21:44:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,6 @@ def consolidate_cart(cart)
   new_cart = {}
   cart.each do |hash|
     hash.each do |name, describe|
-
       if new_cart[name]
         new_cart[name][:count] += 1
 
@@ -24,15 +23,15 @@ def apply_coupons(cart, coupons)
 
 end
 
-def apply_clearance(cart)
-  # new_cart = cart
-  # cart.each do |name, hash|
-  #     if hash[:clearance] #if clearance is true, take 20% off
-  #       new_cart[name][:price] = (cart[name][:price] * 0.8).round(2)
-  #     end
-  # end
-  # new_cart #if not, just return the same cart
-end
+# def apply_clearance(cart)
+#   # new_cart = cart
+#   # cart.each do |name, hash|
+#   #     if hash[:clearance] #if clearance is true, take 20% off
+#   #       new_cart[name][:price] = (cart[name][:price] * 0.8).round(2)
+#   #     end
+#   # end
+#   # new_cart #if not, just return the same cart
+# end
 
 # def checkout(cart, coupons)
 #   # code here
